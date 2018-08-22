be5778b69e83fb13b6dd8d2693f38b73cdfb0bc6
Elephantxx <29962677@qq.com>
2018-08-22 16:43:09 +0800

    字典与字符串

---
+++ 05_高级数据类型/x_12_字典的遍历.py
@@ -0,0 +1,9 @@
+xiaoming_dict = {"name": "小明",
+                 "qq": "123456",
+                 "phone": "10086"}
+# 迭代遍历字典
+# 变量 k 是每一次循环中，获取到键值对 key 的值
+for k in xiaoming_dict:
+
+    print("%s - %s" % (k, xiaoming_dict[k]))
+    
